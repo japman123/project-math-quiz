ab3f0339c388e0da6d9528da4cdd1b6b685b13f2
japman123 <87320271+japman123@users.noreply.github.com>
2022-07-29 13:23:02 -0400

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -2,5 +2,5 @@ function addUser() {
    user_name = document.getElementById("p1").value;
    user_name = document.getElementById("p2").value;
    localStorage.setItem("user_name", user_name)
-   window.location = "game_page.html"
+   window.location = "quiz_game_page.html"
 }
